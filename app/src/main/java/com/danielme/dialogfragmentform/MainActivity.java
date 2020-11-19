51247384531679d77ae7f1b8efe1112f1ab5cf4f
dani <danielme_com@yahoo.com>
2020-11-19 20:20:47 +0100

    formato

--- app/src/main/java/com/danielme/dialogfragmentform/MainActivity.java
+++ app/src/main/java/com/danielme/dialogfragmentform/MainActivity.java
@@ -1,11 +1,11 @@
 package com.danielme.dialogfragmentform;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements FormDialogListener{
 
@@ -29,8 +29,9 @@ public class MainActivity extends AppCompatActivity implements FormDialogListene
   }
 
   @Override
-  public void update(String firstname, String lastname) {
-    textViewFirstName.setText(firstname);
-    textViewLastName.setText(lastname);
+  public void update(String firstName, String lastName) {
+    textViewFirstName.setText(firstName);
+    textViewLastName.setText(lastName);
   }
+
 }
